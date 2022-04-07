ccd538200420941e71c21c58ec4b63d3e3f10612
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-04-08 02:12:00 +0900

    feat: vuexの型指定の変更

---
+++ app/javascript/src/components/UserList.vue
@@ -0,0 +1,40 @@
+<template>
+  <p>{{ userNames }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "@vue/composition-api";
+import {
+  UsersState,
+  UsersGetters,
+  UsersMutations,
+} from "../store/modules/users";
+import {
+  useNamespacedState,
+  useNamespacedGetters,
+  useNamespacedMutations,
+} from "vuex-composition-helpers";
+
+export default defineComponent({
+  setup() {
+    const { users } = useNamespacedState<UsersState>("users", ["users"]);
+    const { userNames } = useNamespacedGetters<UsersGetters>("users", [
+      "userNames",
+    ]);
+
+    const { setUsers } = useNamespacedMutations<UsersMutations>("users", [
+      "setUsers",
+    ]);
+    return {
+      userNames,
+    };
+  },
+});
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
--- app/javascript/src/store/modules/users.ts
+++ app/javascript/src/store/modules/users.ts
@@ -1,23 +1,37 @@
-import { Module, MutationTree, ActionTree } from 'vuex';
+import { Module, MutationTree, ActionTree, GetterTree } from "vuex";
 
 export interface UsersState {
-
+  users: { id: number; name: string }[];
 }
 
 const namespaced = true;
 
 const state: UsersState = {
+  users: [],
 };
 
-const mutations: MutationTree<UsersState> = {
+const getters = {
+  userNames(state: UsersState) {
+    return state.users.map((user) => user.name);
+  },
 };
 
-const actions: ActionTree<UsersState, any> = {
+
+const mutations = {
+  setUsers(state: UsersState, payload: UsersState['users']){
+    state.users = payload
+  }
 };
+
+const actions: ActionTree<UsersState, any> = {};
+
+export type UsersGetters = typeof getters
+export type UsersMutations = typeof mutations
 
 export const users: Module<UsersState, any> = {
   namespaced,
   state,
+  getters,
   actions,
-  mutations
+  mutations,
 };
