980902b8a4474e872d5aabbec8fc848145579ad8
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-05-29 18:09:48 +0900

    fix: chartのオプションのデフォルト値を変更

--- app/javascript/src/components/Chart.vue
+++ app/javascript/src/components/Chart.vue
@@ -16,12 +16,117 @@
 import * as monaco from "monaco-editor";
 import { defineComponent, onMounted, ref } from "@vue/composition-api";
 import { Chart } from "highcharts-vue";
+import Highcharts from "highcharts";
+import seriesLabel from "highcharts/modules/series-label";
+
+const defaultOption = {
+  chart: {
+    type: "column",
+  },
+  credits: {
+    enabled: false,
+  },
+  title: {
+    text: "",
+  },
+  yAxis: [
+    {
+      title: {
+        text: "",
+      },
+      labels: {
+        style: {
+          fontSize: "8px",
+        },
+      },
+    },
+    {
+      title: {
+        text: "",
+      },
+      opposite: true,
+      visible: false,
+    },
+  ],
+  xAxis: {
+    title: {
+      text: "",
+    },
+    categories: ["05/23", "05/24", "05/25", "05/26", "05/27"],
+    labels: {
+      style: {
+        fontSize: "8px",
+      },
+    },
+  },
+  legend: {
+    enabled: false,
+  },
+  series: [
+    {
+      name: "目標",
+      type: "column",
+      data: [
+        {
+          y: 33,
+          color: "#5fbeeb",
+        },
+        {
+          y: 33,
+          color: "#5fbeeb",
+        },
+        {
+          y: 33,
+          color: "#5fbeeb",
+        },
+        {
+          y: 33,
+          color: "#5fbeeb",
+        },
+        {
+          y: 33,
+          color: "#FFC7C4",
+        },
+      ],
+    },
+    {
+      name: "実績",
+      type: "line",
+      data: [29, 29, 29, 29, 45],
+      color: "#FFC7C4",
+      marker: {
+        enabled: false,
+      },
+    },
+  ],
+  plotOptions: {
+    series: {
+      animation: false,
+    },
+  },
+  tooltip: {
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    borderWidth: 2,
+    headerFormat:
+      '<div style="align-items:center;color:{point.color};display:flex;flex-direction:column;gap:2px;">\n              <span style="font-size:8px;letter-spacing:-1px;">{point.key}</span>',
+    pointFormat:
+      '<span style=\\"color:{point.color};font-size:16px\\"><b>{point.y}</b></span>',
+    footerFormat: "</div>",
+    shadow: false,
+    shape: "square",
+    stickOnContact: true,
+    useHTML: true,
+  },
+};
 
 export default defineComponent({
   components: {
     highcharts: Chart,
   },
   setup() {
+    seriesLabel(Highcharts);
+
     let editor: monaco.editor.IStandaloneCodeEditor | null = null;
 
     onMounted(() => {
@@ -29,11 +134,11 @@ export default defineComponent({
 
       editor = monaco.editor.create(editorElement, {
         value: chartOption.value,
-        language: "javascript",
+        language: "json",
       });
     });
 
-    const chartOption = ref("{series: [{data: [1,2,3]}]}");
+    const chartOption = ref(JSON.stringify(defaultOption, null, 2));
 
     const onClickApply = () => {
       const chatOptionString = editor.getModel().getValue();
@@ -43,13 +148,7 @@ export default defineComponent({
       eval(applyCommand);
     };
 
-    const chartOptions = ref({
-      series: [
-        {
-          data: [1, 2, 3],
-        },
-      ],
-    });
+    const chartOptions = ref(defaultOption);
 
     return { chartOptions, onClickApply };
   },
