2ac49ce50a568c6df896c652f7aff3fedec1847b
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-06-11 01:13:10 +0900

    feat: defer component

---
+++ app/javascript/src/components/Defer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="defer">
+    <slot :defer="defer"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import { useDefer } from "../composables/useDefer";
+
+export default defineComponent({
+  setup() {
+    const { defer } = useDefer();
+    return { defer };
+  },
+});
+</script>
