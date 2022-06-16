49c702b4dd7ac2ff36b5944dc7ee9c89ea645e1b
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-06-17 00:57:40 +0900

    fix: 自作のD&Dコンポーネントの作成

--- app/javascript/src/components/Sortable.vue
+++ app/javascript/src/components/Sortable.vue
@@ -1,26 +1,71 @@
 <template>
   <div class="container">
-    <ul id="items">
-      <li class="list-item">item 1</li>
-      <li class="list-item">item 2</li>
-      <li class="list-item">item 3</li>
+    <ul id="items" v-for="item in items" :key="item.id">
+      <sort-item
+        :name="item.label"
+        @dragstart="onDragStart($event, item)"
+        @dragenter="onDragEnter($event, item)"
+        @dragover="onDragOver($event, item)"
+      ></sort-item>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "@vue/composition-api";
+import { defineComponent, onMounted, ref, Ref } from "@vue/composition-api";
 // Default SortableJS
-import Sortable from "sortablejs";
+import Sortable from "sortablejs/modular/sortable.esm.js";
+import SortItem from "./Sortable/SortItem.vue";
+
+type Item = { id: number; label: string };
 
 export default defineComponent({
+  components: { SortItem },
   setup() {
-    onMounted(() => {
-      var el = document.getElementById("items");
-      var sortable = Sortable.create(el, {
-        animation: 150,
-      });
-    });
+    const items: Item[] = [
+      { id: 1, label: "Item1" },
+      { id: 2, label: "Item2" },
+      { id: 3, label: "Item3" },
+    ];
+
+    /** D&Dで掴んでいる要素 */
+    const draggingItem: Ref<Item | undefined> = ref();
+
+    /** D&Dでホバー中の要素 */
+    const dragTargetItem: Ref<Item | undefined> = ref();
+
+    /** D&Dでホバー中の要素についてマウスポインタが上半分にあるか、下半分にあるか*/
+    const dragTargetPosition: Ref<"up" | "down" | undefined> = ref();
+
+    function onDragStart(_: DragEvent, item: Item) {
+      draggingItem.value = item;
+    }
+
+    function onDragEnter(_: DragEvent, item: Item) {
+      dragTargetItem.value = item;
+    }
+
+    function onDragOver(evt: DragEvent) {
+      const mouseY = evt.clientY;
+
+      const target = evt.target as HTMLElement;
+      const targetRect = target.getClientRects()[0];
+
+      if (mouseY < targetRect.y + targetRect.height / 2) {
+        dragTargetPosition.value = "up";
+      } else {
+        dragTargetPosition.value = "down";
+      }
+    }
+
+    return {
+      items,
+      onDragStart,
+      onDragEnter,
+      onDragOver,
+      draggingItem,
+      dragTargetItem,
+    };
   },
 });
 </script>
---
+++ app/javascript/src/components/Sortable/SortItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="list-item"
+    draggable="true"
+    @dragstart="$emit('dragstart', $event)"
+    @dragenter="onDragEnter"
+    @dragover="onDragOver"
+  >
+    {{ name }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "@vue/composition-api";
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+    },
+  },
+  setup(_, { emit }) {
+    function onDragEnter(event: DragEvent) {
+      event.preventDefault();
+      emit("dragenter", event);
+    }
+
+    function onDragOver(event: DragEvent) {
+      event.preventDefault();
+      emit("dragover", event);
+    }
+
+    return {
+      onDragEnter,
+      onDragOver,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.list-item {
+  position: relative;
+  display: block;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: -1px;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+}
+</style>
