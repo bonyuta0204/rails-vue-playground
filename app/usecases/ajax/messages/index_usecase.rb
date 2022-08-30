b80538fb4947c8ca9f6b5152a274c4a90fb39119
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-08-31 00:57:53 +0900

    feat: add message and channel ajax api

---
+++ app/usecases/ajax/messages/index_usecase.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module Messages
+    # Messages Index usecase
+    class IndexUsecase
+      def initialize(params)
+        @channel_id = params[:channel_id]
+      end
+
+      def call
+        messages = ::Messages::IndexService.new(@channel_id).execute
+        [:ok, MessageBlueprint.render_as_json(messages)]
+      end
+    end
+  end
+end
