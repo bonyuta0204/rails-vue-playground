2c6bb2198a52cf5d59715c983bc4543be4a28601
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2022-09-01 00:51:57 +0900

    fix: message list bugs

--- app/controllers/ajax/message_lists_controller.rb
+++ app/controllers/ajax/message_lists_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Api
+module Ajax
   # messages controller
   class MessageListsController < ApplicationController
     def index
--- app/usecases/ajax/message_lists/index_usecase.rb
+++ app/usecases/ajax/message_lists/index_usecase.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Api
+module Ajax
   module MessageLists
     # Messages Index usecase
     class IndexUsecase
